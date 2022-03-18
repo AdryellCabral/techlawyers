d96e058576844bdf669113364e30c8a855e16786
Adryell Cabral <adryellluciano@gmail.com>
2022-03-18 09:23:29 -0300

    chore: adding cors config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import appRouter from "./routes";
 import { config } from "dotenv";
+import cors from "cors";
 
 config();
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api", appRouter);
 
 const porta = process.env.PORT;
